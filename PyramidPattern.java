dec11b9a2e127e7db27bfd5648a65ca090a34ac5
Yash Jadoun <79275231+yashsinghjadoun@users.noreply.github.com>
2022-02-20 15:47:18 +0530

    Add files via upload

---
+++ PyramidPattern.java
@@ -0,0 +1,14 @@
+class PyramidPattern
+{
+	public static void main(String yash[])
+	{
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}

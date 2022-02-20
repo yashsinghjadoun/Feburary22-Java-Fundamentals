466ec0bec8774ace51d5bcc58d01fc2fa6c3d381
Yash Jadoun <79275231+yashsinghjadoun@users.noreply.github.com>
2022-02-20 15:41:14 +0530

    Add files via upload

---
+++ LeftTraingleStarPattern.java
@@ -0,0 +1,14 @@
+class LeftTraingleStarPattern
+{
+	public static void main(String yash[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

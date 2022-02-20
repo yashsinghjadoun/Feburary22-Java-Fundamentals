d99cf8e27fefa50b12d62305ffcd786a284afb5d
Yash Jadoun <79275231+yashsinghjadoun@users.noreply.github.com>
2022-02-20 16:09:38 +0530

    Add files via upload

---
+++ Programtoprintsumofsquaresupto10.java
@@ -0,0 +1,14 @@
+class Programtoprintsumofsquaresupto10
+{
+	public static void main(String yash[])
+	{
+		int sum=0;
+		int i=1;
+		while(i<=10)
+		{
+			sum+=(i*i);
+			i++;
+		}
+		System.out.println(sum);
+	}
+}

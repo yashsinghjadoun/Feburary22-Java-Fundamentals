5da2e2af97ed85056ccfd2fbcf5e8c14ed566cf1
Yash Jadoun <79275231+yashsinghjadoun@users.noreply.github.com>
2022-02-20 16:01:07 +0530

    Add files via upload

---
+++ Programtoprintnumbersdivisibleby3from1to100.java
@@ -0,0 +1,12 @@
+class Programtoprintnumbersdivisibleby3from1to100
+{
+	public static void main(String yash[])
+	{
+		int i=1;
+		while(i<=100)
+		{
+			if(i%3==0)System.out.println(i);
+			i++;
+		}
+	}
+}
